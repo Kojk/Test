75d71d71ff90db66d50c872846e0a1ed95ccabc8
tgorb <tgorb@LAPTOP-G9K5KA6J>
2017-12-29 14:44:17 +0100

    Add project files.

---
+++ Test/otazky.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class otazky
+    {
+        public string otazka1 = "Kolko stran ma kocka?";
+        public string otazka2 = "Kolko prepon ma trojuholnik?";
+        public string otazka3 = "Ak je priemer kruhu 8 kolko je jeho polomer? ";
+        public string otazka4 = "Kolko je odmocnina z 9";
+        public string otazka5 = "Kolko stupnov ma pravy uhol?";
+        public string otazka6 = "Moze sa delit nulou?";
+        public string otazka7 = "Kolko sekund ma hodina?";
+        public string otazka8 = "Kolko stran ma sesthran?";
+        public string otazka9 = "Ked slimak prejde za bodu A do bodu B za 30 minut, kolko bude trvat trom slimakom prejst z bodu A do ´bodu B?";
+        public string otazka10 = "Ako sa vola podstava valca?";
+    }
+}
---
+++ Test/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class test
+    {
+        string[] simpleChoice = new string[5] { "Kolko stran ma kocka?","Kolko prepon ma trojuholnik?","Ak je priemer kruhu 8 kolko je jeho polomer? ","Kolko je odmocnina z 9","Kolko stupnov ma pravy uhol?"};
+        string[] multipleChoice = new string[5] {"Moze sa delit nulou?","Kolko sekund ma hodina?","Kolko stran ma sesthran?","Ked slimak prejde za bodu A do bodu B za 30 minut, kolko bude trvat trom slimakom prejst z bodu A do ´bodu B?","Ako sa vola podstava valca?"};
+    }
+}
